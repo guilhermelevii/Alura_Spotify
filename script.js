5eb2bb4dafa1d7cfcb11630eb6571e11dc1cf06c
Guilherme Levi <134446038+guilhermelevii@users.noreply.github.com>
2024-01-29 13:23:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,26 @@ function requestApi(searchTerm) {
         .then((result) => displayResults(result))
 }
 
+function displayResults(result) {
+    resultPlaylist.classList.add('hidden')
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
 
 document.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
+        resultPlaylist.classList.remove('hidden');
+        resultArtist.classList.add('hidden');
         return
     }
+    
+    requestApi(searchTerm);
+})
